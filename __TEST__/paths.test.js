24d0e3f43ce043a4322dbc54d55c1e7b089a5c19
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2022-01-12 14:03:31 -0600

    Prueba unitaria: verificar rutas

---
+++ __TEST__/paths.test.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require("fs");
+const newPaths = require("../packages/rob-scripts/config/news-paths");
+const { resolveApp } = require("../packages/rob-scripts/config/reselve-app-path");
+const appDirectory = fs.realpathSync(process.cwd());
+
+const testConfig = {
+  pathsFiles: {
+    appIndexJs: "main.js",
+    appHtml: "public/putas.html",
+  },
+};
+
+const indexJs = path.resolve(
+  appDirectory,
+  `packages/template/${testConfig.pathsFiles.appIndexJs}`
+);
+test("Validar rutas del appIndexJs", () => {
+  expect(newPaths(testConfig.pathsFiles,true).appIndexJs).toBe(indexJs);
+});
+
+test("Validar ruta de appHtml", () => {
+  const html = path.resolve(
+    appDirectory,
+    `packages/template/${testConfig.pathsFiles.appHtml}`
+  );
+  expect(newPaths(testConfig.pathsFiles,true).appHtml).toBe(html);
+});
+
+test("Validar ruta de appIndexJs de produccion", () => {
+  expect(newPaths(testConfig.pathsFiles,false).appIndexJs).toBe(indexJs);
+});
