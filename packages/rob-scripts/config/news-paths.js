a97f737f9a5b4e44bf4cb9d4d6825c6e1825b4b2
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2022-01-12 14:04:27 -0600

    Refactorizando news-paths func v1

--- packages/rob-scripts/config/news-paths.js
+++ packages/rob-scripts/config/news-paths.js
@@ -1,19 +1,18 @@
-const isTempleteRun = require("./is-templete-run");
-const paths = require("./paths");
 const { resolveOwn, resolveApp } = require("./reselve-app-path");
 
-module.exports = (config) => {
-    const configKeys = Object.keys(config);
-    const configValues = Object.values(config);
-    
-    configKeys.forEach((key,i) => {
-      if(isTempleteRun()){
-        const templatePath = "../template";
-        config[key] = resolveOwn(`${templatePath}/${configValues[i]}`);
-        return
-      }
-      config[key] = resolveApp(configValues[i]);
-    
-    })
-    return config;
-}
+module.exports = (config, isTempleteRun) => {
+  const newConfig = config;
+  const configKeys = Object.keys(newConfig);
+  const configValues = Object.values(newConfig);
+  console.log(resolveApp(`${configValues[0]}`));
+  configKeys.forEach((key, i) => {
+    if (isTempleteRun) {
+      const templatePath = "../template";
+      newConfig[key] = resolveOwn(`${templatePath}/${configValues[i]}`);
+      return;
+    }
+    console.log("Paths mode is not templete run");
+    newConfig[key] = resolveApp(`${configValues[i]}`);
+  });
+  return newConfig;
+};
