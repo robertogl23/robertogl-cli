e06164a60a0fb5293a56a3a970517fc1624fad34
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2022-01-16 13:37:44 -0600

    Refactor v1 NewPaths

--- packages/rob-scripts/config/news-paths.js
+++ packages/rob-scripts/config/news-paths.js
@@ -2,17 +2,16 @@ const { resolveOwn, resolveApp } = require("./reselve-app-path");
 
 module.exports = (config, isTempleteRun) => {
   const newConfig = config;
-  const configKeys = Object.keys(newConfig);
-  const configValues = Object.values(newConfig);
-  console.log(resolveApp(`${configValues[0]}`));
-  configKeys.forEach((key, i) => {
+  const entries = Object.entries(newConfig);
+
+  entries.forEach(([key, value]) => { 
     if (isTempleteRun) {
       const templatePath = "../template";
-      newConfig[key] = resolveOwn(`${templatePath}/${configValues[i]}`);
+      newConfig[key] = resolveOwn(`${templatePath}/${value}`);
       return;
     }
-    console.log("Paths mode is not templete run");
-    newConfig[key] = resolveApp(`${configValues[i]}`);
+    newConfig[key] = resolveApp(`${value}`);
   });
+
   return newConfig;
 };
