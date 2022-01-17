a5f120579bd974f7570a5565d5a5903b8f984456
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2022-01-17 12:00:14 -0600

    Configuracion de plugins y CSSModules

--- packages/rob-scripts/config/webpack.config.js
+++ packages/rob-scripts/config/webpack.config.js
@@ -1,13 +1,20 @@
 "use strict";
+const fs = require("fs");
+const path = require("path");
 const pathsConfig = require("./paths");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin").default;
 const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 const MyPlugin = require("../utils/hello-word-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
+const getCSSModuleLocalIdent = require("../utils/getCSSModuleLocalIdent");
+
+// style files regexes
 const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+
 let paths = pathsConfig;
 
 const swcOptions = {
@@ -28,71 +35,77 @@ const swcOptions = {
   },
 };
 
-const plugins = (isEnvProduction,) => [
-  new HtmlWebpackPlugin(
-    Object.assign(
-      {},
-      {
-        inject: true,
-        template: paths.appHtml,
-      },
-      isEnvProduction
-        ? {
-            minify: {
-              removeComments: true,
-              collapseWhitespace: true,
-              removeRedundantAttributes: true,
-              useShortDoctype: true,
-              removeEmptyAttributes: true,
-              removeStyleLinkTypeAttributes: true,
-              keepClosingSlash: true,
-              minifyJS: true,
-              minifyCSS: true,
-              minifyURLs: true,
-            },
-          }
-        : undefined
-    )
-  ),
-  new MiniCssExtractPlugin({
-    filename: "static/css/[name].[contenthash:8].css",
-    chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
-  }),
-
-  new WebpackManifestPlugin({
-    fileName: "asset-manifest.json",
-    publicPath: paths.publicUrlOrPath,
-    generate: (seed, files, entrypoints) => {
-      const manifestFiles = files.reduce((manifest, file) => {
-        manifest[file.name] = file.path;
-        return manifest;
-      }, seed);
-      const entrypointFiles = entrypoints.main.filter(
-        (fileName) => !fileName.endsWith(".map")
-      );
-
-      return {
-        files: manifestFiles,
-        entrypoints: entrypointFiles,
-      };
-    },
-  }),
-];
-const getPlugins = (isProduction) =>
-  isProduction
-    ? [
-        ...plugins,
-        new WorkboxPlugin.GenerateSW({
-          clientsClaim: true,
-          skipWaiting: true,
-        }),
-        new MyPlugin({ options: "" }),
-      ]
-    : plugins(isProduction);
-
 module.exports = function (webpackEnv, robConfig) {
   const isEnvDevelopment = webpackEnv === "development";
   const isEnvProduction = webpackEnv === "production";
+
+  const getStyleLoaders = (cssOptions, preProcessor) => {
+    const loaders = [
+      isEnvDevelopment && require.resolve("style-loader"),
+      isEnvProduction && {
+        loader: MiniCssExtractPlugin.loader,
+        // css is located in `static/css`, use '../../' to locate index.html folder
+        // in production `paths.publicUrlOrPath` can be a relative path
+        options: paths.publicUrlOrPath.startsWith(".")
+          ? { publicPath: "../../" }
+          : {},
+      },
+      {
+        loader: require.resolve("css-loader"),
+        options: cssOptions,
+      },
+      {
+        // Options for PostCSS as we reference these options twice
+        // Adds vendor prefixing based on your specified browser support in
+        // package.json
+        loader: require.resolve("postcss-loader"),
+        options: {
+          postcssOptions: {
+            // Necessary for external CSS imports to work
+            // https://github.com/facebook/create-react-app/issues/2677
+            ident: "postcss",
+            config: false,
+            plugins: [
+              "postcss-flexbugs-fixes",
+              [
+                "postcss-preset-env",
+                {
+                  autoprefixer: {
+                    flexbox: "no-2009",
+                  },
+                  stage: 3,
+                },
+              ],
+              // Adds PostCSS Normalize as the reset css with default options,
+              // so that it honors browserslist config in package.json
+              // which in turn let's users customize the target behavior as per their needs.
+              "postcss-normalize",
+            ],
+          },
+          sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
+        },
+      },
+    ].filter(Boolean);
+    if (preProcessor) {
+      loaders.push(
+        {
+          loader: require.resolve("resolve-url-loader"),
+          options: {
+            sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
+            root: paths.appSrc,
+          },
+        },
+        {
+          loader: require.resolve(preProcessor),
+          options: {
+            sourceMap: true,
+          },
+        }
+      );
+    }
+    return loaders;
+  };
+
   if (!robConfig) {
     return {
       mode: isEnvProduction ? "production" : isEnvDevelopment && "development",
@@ -121,6 +134,7 @@ module.exports = function (webpackEnv, robConfig) {
     };
   }
   paths = Object.assign(paths, robConfig);
+  console.log("publicUrlOrPath: ", paths.publicUrlOrPath);
   return {
     mode: isEnvProduction ? "production" : isEnvDevelopment && "development",
     devtool: isEnvProduction ? false : "cheap-module-source-map",
@@ -128,7 +142,7 @@ module.exports = function (webpackEnv, robConfig) {
     output: {
       path: paths.appBuild,
       pathinfo: isEnvDevelopment,
-      publicPath: "",
+      publicPath: paths.publicUrlOrPath,
       filename: isEnvProduction
         ? "static/js/[name].[contenthash:8].js"
         : isEnvDevelopment && "static/js/[name].js",
@@ -187,14 +201,35 @@ module.exports = function (webpackEnv, robConfig) {
             options: swcOptions,
           },
         },
+
         {
           test: cssRegex,
-          use: [
-            isEnvDevelopment
-              ? "style-loader"
-              : { loader: MiniCssExtractPlugin.loader },
-            "css-loader",
-          ],
+          exclude: cssModuleRegex,
+          use: getStyleLoaders({
+            importLoaders: 1,
+            sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
+            modules: {
+              mode: "icss",
+            },
+          }),
+          // Don't consider CSS imports dead code even if the
+          // containing package claims to have no side effects.
+          // Remove this when webpack adds a warning or an error for this.
+          // See https://github.com/webpack/webpack/issues/6571
+          sideEffects: true,
+        },
+        // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+        // using the extension .module.css
+        {
+          test: cssModuleRegex,
+          use: getStyleLoaders({
+            importLoaders: 1,
+            sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
+            modules: {
+              mode: "local",
+              getLocalIdent: getCSSModuleLocalIdent,
+            },
+          }),
         },
         {
           type: "asset",
@@ -202,6 +237,61 @@ module.exports = function (webpackEnv, robConfig) {
         },
       ],
     },
-    plugins: getPlugins(isEnvProduction),
+    plugins: [
+      new HtmlWebpackPlugin(
+        Object.assign(
+          {},
+          {
+            inject: true,
+            template: paths.appHtml,
+          },
+          isEnvProduction
+            ? {
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
+            : undefined
+        )
+      ),
+      new WebpackManifestPlugin({
+        fileName: "asset-manifest.json",
+        publicPath: paths.publicUrlOrPath,
+        generate: (seed, files, entrypoints) => {
+          const manifestFiles = files.reduce((manifest, file) => {
+            manifest[file.name] = file.path;
+            return manifest;
+          }, seed);
+          const entrypointFiles = entrypoints.main.filter(
+            (fileName) => !fileName.endsWith(".map")
+          );
+
+          return {
+            files: manifestFiles,
+            entrypoints: entrypointFiles,
+          };
+        },
+      }),
+      !isEnvDevelopment &&
+        new MiniCssExtractPlugin({
+          filename: "static/css/[name].[contenthash:8].css",
+          chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+        }),
+      isEnvProduction &&
+        new WorkboxPlugin.GenerateSW({
+          clientsClaim: true,
+          skipWaiting: true,
+        }),
+      isEnvProduction && new MyPlugin({ options: "" }),
+    ].filter(Boolean),
   };
 };
